bb686fc9d189d6ac29ba9f9762362f1728ce6e80
SHOU_HU_SHEN <SHOU_HU_SHEN@users.noreply.gitee.com>
2026-08-01 02:00:02 +0800

    deploy knowledge map

--- gitee-config.js
+++ gitee-config.js
@@ -1,8 +1,8 @@
-﻿// Gitee 云同步配置
+﻿// Gitee 云同步配置 + EdgeOne Pages 站点地址
 window.GITEE_CONFIG = {
   siteOwner: "SHOU_HU_SHEN",
   siteRepo: "computer-org-map",
-  siteUrl: "",
+  siteUrl: "https://computer-org-map-hytfk98v.edgeone.dev?eo_token=b3b4d1f7c5a308a8d84a11de31084db6&eo_time=1785520525",
   dataOwner: "SHOU_HU_SHEN",
   dataRepo: "computer-org-data",
   branch: "master",
